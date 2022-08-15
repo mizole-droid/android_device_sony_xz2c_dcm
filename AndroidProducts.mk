8531189b315916b21e93948337839b5447592ea8
abcer 310 <chaliz@MacBook-Pro-2020.local>
2022-08-16 00:36:55 +0900

    AICPfy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_xz2c.mk
+    $(LOCAL_DIR)/aicp_xz2c.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_xz2c-user \
-    lineage_xz2c-userdebug \
-    lineage_xz2c-eng
+    aicp_xz2c-user \
+    aicp_xz2c-userdebug \
+    aicp_xz2c-eng
