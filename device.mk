0a4e1c80d6edb4428d8116826147c3cc0e3bf824
derfelot <derfelot.git@gmail.com>
2023-04-24 00:15:20 +0530

    [PATCH] xz2c_dcm : Build missing nfc vendor libraries

--- device.mk
+++ device.mk
@@ -51,3 +51,7 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2.vendor
